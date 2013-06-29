8c417eb50898a7919d8cb4d449364736e4831a58
Zak Johnson <zakj@nox.cx>
2013-06-29 13:22:19 -0700

    Update production instance path

--- neu/__init__.py
+++ neu/__init__.py
@@ -1,3 +1,5 @@
+import os.path
+
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.restful import Api
@@ -9,9 +11,15 @@ api_mediatype = 'application/neu.v0+json'
 restful = Api(prefix='/api', default_mediatype=api_mediatype)
 restful.representations[api_mediatype] = json.output_json
 
+PRODUCTION_INSTANCE_PATH = '/srv/www/instance'
+
 
 def create_app():
-    app = Flask(__name__, instance_relative_config=True)
+    instance_path = None
+    if os.path.exists(PRODUCTION_INSTANCE_PATH):
+        instance_path = PRODUCTION_INSTANCE_PATH
+    app = Flask(
+        __name__, instance_relative_config=True, instance_path=instance_path)
     #app.config.update(...)
     app.config.from_pyfile('settings.cfg', silent=True)
     # TODO: Exceptional/Airbrake/Raygun/etc.
